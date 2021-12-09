ef18363667952d68577011f837a525d831637ebc
rgcivit <86114994+rgcivit@users.noreply.github.com>
2021-12-09 11:46:45 -0300

    change

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -6,7 +6,6 @@ import { useParams } from 'react-router';
 export function getVgames(){
     return async function(dispatch){
      const json= await axios.get("http://localhost:3001/videogames",{
-
         });
         return dispatch({
             type: 'GET_VIDEOGAMES',
