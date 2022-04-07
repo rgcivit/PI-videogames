873907d9ed922696ba5b2b7f9cd50c7673cd7085
rgcivit <86114994+rgcivit@users.noreply.github.com>
2022-04-07 17:16:43 -0300

    merge

--- api/src/app.js
+++ api/src/app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
-const cors = require('cors');
 
 require('./db.js');
 
 const server = express();
+const cors = require('cors');
 
 server.name = 'API';
 
