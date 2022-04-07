f0f1a0c54b594228c78382a340c1acbaa573471c
rgcivit <86114994+rgcivit@users.noreply.github.com>
2022-04-07 14:22:43 -0300

    comill

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 
 export function getVgames(){
     return async function(dispatch){
-     const json= await axios.get(`/videogames`)
+     const json= await axios.get("/videogames")
        
         return dispatch({
             type: 'GET_VIDEOGAMES',
@@ -23,7 +23,7 @@ export  function sortvgames(payload) {
 export function getNameVgames (name){
     return async function(dispatch){
         try {
-            var json=await axios.get(`/videogames?name=`+ name)
+            var json=await axios.get("/videogames?name="+ name)
         return dispatch({
             type:'GET_NAME_VGAMES',
             payload:json.data
@@ -44,7 +44,7 @@ export function filterCreated (payload){
 }
 export function getGenres(){
     return async function(dispatch){
-        var info= await axios(`/genres`,{
+        var info= await axios('/genres',{
        
         })
         return dispatch({
@@ -55,7 +55,7 @@ export function getGenres(){
 }
 export function getPlatforms(){
     return async function(dispatch){
-        var info= await axios(`/platform`,{
+        var info= await axios('/platform',{
 
         })
     return dispatch({type:'GET_PLATFORMS', payload:info.data})
@@ -63,7 +63,7 @@ export function getPlatforms(){
 }
 export function postVgame(payload){
     return async function (dispatch){
-        const response= await axios.post(`/videogames`,payload);
+        const response= await axios.post('/videogames',payload);
        
         return response;
     }
